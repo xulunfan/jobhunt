50ab2f3fefe516784202d7e34775901a660a58cf
Xu <xulunfan@users.noreply.github.com>
2018-08-15 20:02:00 +0800

    Update super-ugly-number.cpp

--- super-ugly-number.cpp
+++ super-ugly-number.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        if(n==1) return 1;
         int a[101]={0};
         vector<int>v;
         v.push_back(1);
         while(v.size()<n){
-            vector<int>b;
-            for(int i=0;i<primes.size();i++){
-                b.push_back(primes[i]*v[a[i]]);
-            }
-            int result=b[0];
-            
-            for(int i=0;i<b.size();i++) result=min(result,b[i]);
+            int result=INT_MAX;      
+            for(int i=0;i<primes.size();i++) result=min(result,primes[i]*v[a[i]]);
             v.push_back(result);
-            for(int i=0;i<b.size();i++){
-                if(result==b[i]) a[i]++;
+            for(int i=0;i<primes.size();i++){
+                if(result==primes[i]*v[a[i]]) a[i]++;
             }            
         }
         return v[n-1];
